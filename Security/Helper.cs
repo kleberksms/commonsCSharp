626ba954fa3578912e3849230ef81b9085a4b42b
Kleber <sistemas.kleber@bellinatiperez.com.br>
2016-04-11 13:53:03 -0300

    Security Helper Sha256 e md5

---
+++ Security/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Security
+{
+    public static class Helper
+    {
+        public static string GetSha256Hash(string input)
+        {
+            HashAlgorithm hashAlgorithm = new SHA256CryptoServiceProvider();
+
+            byte[] byteValue = System.Text.Encoding.UTF8.GetBytes(input);
+
+            byte[] byteHash = hashAlgorithm.ComputeHash(byteValue);
+
+            return Convert.ToBase64String(byteHash);
+        }
+
+        public static string GetMd5Hash(string input)
+        {
+            var md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
