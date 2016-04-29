5181b476fa08cd1d13bfd4fb996bdb32c17519be
Kleber <sistemas.kleber@bellinatiperez.com.br>
2016-04-29 10:09:26 -0300

    email test

---
+++ Validation.Test/EmailTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace Validation.Test
+{
+    [TestFixture]
+    public class EmailTest
+    {
+        [Test]
+        public void TestValidEmail()
+        {
+            var e = "someone@somewhere.com";
+            var email = new Email(e);
+            Assert.IsTrue(email.IsValid());
+        }
+
+        [Test]
+        public void TestInvalidEmail()
+        {
+            var e = "someone@somewhere.";
+            var email = new Email(e);
+            Assert.IsFalse(email.IsValid());
+        }
+
+        [Test]
+        public void TestInvalidEmailDomain()
+        {
+            var e = "someone@poxqts.com";
+            var email = new Email(e);
+            email.validDomain = true;
+            Assert.IsFalse(email.IsValid());
+        }
+
+        [Test]
+        public void TestValidEmailDomain()
+        {
+            var e = "someone@gmail.com";
+            var email = new Email(e);
+            email.validDomain = true;
+            Assert.IsTrue(email.IsValid());
+        }
+    }
+}
---
+++ Validation/Email.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace Validation
+{
+    public class Email
+    {
+        private string _email;
+        public bool validDomain = false;
+
+        public Email(string email)
+        {
+            _email = email;
+        }
+
+        public bool IsValid()
+        {
+            var ret = false;
+
+            string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            if (validDomain)
+            {
+                MailAddress address = new MailAddress(_email);
+                string host = address.Host;
+                IPHostEntry resolve;
+                try
+                {
+                    resolve = Dns.GetHostEntry(host);
+                }
+                catch (SocketException ex)
+                {
+                    return false;
+                }
+            }
+
+            return  regex.IsMatch(_email);
+        }
+    }
+}
